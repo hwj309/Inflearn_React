bff17d4ada947c2e8497712ddac6e78bb5ff6a0e
hwj309 <99112387+hwj309@users.noreply.github.com>
2024-07-25 17:05:14 +0900

    줄정리

--- src/chapter_05/CommentList.jsx
+++ src/chapter_05/CommentList.jsx
@@ -1,21 +1,6 @@
 import React, { useState, useRef } from "react";
 import Comment from "./Comment";
 
-const comments = [
-    {
-        name: "황선재",
-        comment: "안녕하세요. 황선재 입니다."
-    },
-    {
-        name: "유재석",
-        comment: "안녕하세요. 유재석 입니다."
-    },
-    {
-        name: "강민경",
-        comment: "안녕하세요. 강민경 입니다."
-    }
-]
-
 function CommentList(props) {
     const [commentss, setCommentss] = useState([]);
 
@@ -26,8 +11,6 @@ function CommentList(props) {
         let copy = [...commentss];
         copy = [...commentss, { name: nameRef.current, comment: commentRef.current}];
         setCommentss(copy);
-
-        
     }
 
     return (
@@ -39,17 +22,12 @@ function CommentList(props) {
             <input id="comment" ref={commentRef} onChange={(e) => {commentRef.current = e.target.value}}></input>
             <button onClick={() => {addInfo()}}>추가</button>
             {
-            // comments.map((comment) => {
-            //     return (
-            //     <Comment name={comment.name} comment={comment.comment}/>
-            //     );
-            // })
-            commentss.map((comm, i) => {
-                return (
-                    <Comment key={i} name={comm.name} comment={comm.comment}/>
-                );
-            })
-        }
+                commentss.map((comm, i) => {
+                    return (
+                        <Comment key={i} name={comm.name} comment={comm.comment}/>
+                    );
+                })
+            }
         </div>
     );
 }
